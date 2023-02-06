e83674cd7e0b23110b11caa2a3125e3d1b17df18
ses1234567890 <ses1234567890@hotmail.es>
2023-02-06 20:05:35 +0000

    develop branch created

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,56 +2,61 @@ import React, { Component } from "react";
 
 export const Footer = () => (
   <footer className="text-center text-lg-start bg-white text-muted">
-    <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-      <div className="me-5 d-none d-lg-block">
-        <span>Conecta con nosotros en nuestras redes sociales:</span>
-      </div>
-      <div>
-        <a href="" className="me-4 link-secondary">
-          <i class="fab fa-facebook-f"></i>
-        </a>
-        <a href="" className="me-4 link-secondary">
-          <i class="fab fa-twitter"></i>
-        </a>
-        <a href="" className="me-4 link-secondary">
-          <i class="fab fa-instagram"></i>
-        </a>
-        <a href="" className="me-4 link-secondary">
-          <i className="fab fa-linkedin"></i>
-        </a>
-      </div>
-    </section>
     <section className="">
       <div className="container text-center text-md-start mt-5">
         <div className="row mt-3">
-          <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-            <h6 className="text-uppercase fw-bold mb-4">
-              <i className="fas fa-gem me-3 text-secondary"></i>GeoCaching
-            </h6>
-            <p>
+          <div className="col-3  mx-auto mb-4">
+            <div className="row text-start d-flex">
+              <h6 className="text-uppercase fw-bold mb-4">
+
+                <i className="fas fa-gem me-2 text-secondary justify-content-start"></i>GeoCaching
+              </h6>
+            </div>
+            <div className="row ps-3 pe-2">
               Bienvenido a la comunidad de encuentra tesoros más grande del
               Planeta Tierra.
-            </p>
+
+            </div>
+
           </div>
-          <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-            <h6 className="text-uppercase fw-bold mb-4">Sobre Geocahing</h6>
-            <p>
+          <div className="col-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <div className="row">
+              <h6 className="text-uppercase fw-bold mb-4">Sobre Geocahing</h6>
+            </div>
+            <div className="row">
               <a href="#!" className="text-reset">
                 Preguntas frecuentes
               </a>
-            </p>
-            <p>
+            </div>
+            <div className="row">
               <a href="#!" className="text-reset">
                 Empleo
               </a>
-            </p>
-            <p>
+            </div>
+            <div className="row">
               <a href="#!" className="text-reset">
                 Prensa
               </a>
-            </p>
+            </div>
           </div>
-          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <h6 className="text-uppercase fw-bold mb-4">Redes Sociales</h6>
+            <p>
+              <a href="" className="me-4 link-secondary">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </p>
+            <a href="" className="me-4 link-secondary">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a href="" className="me-4 link-secondary">
+              <i class="fab fa-instagram"></i>
+            </a>
+            <a href="" className="me-4 link-secondary">
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </div>
+          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
             <h6 className="text-uppercase fw-bold mb-4">Contacto</h6>
             <p>
               <i className="fas fa-home me-3 text-secondary"></i>España
@@ -64,11 +69,5 @@ export const Footer = () => (
         </div>
       </div>
     </section>
-    <div className="text-center p-4">
-      © 2022 Copyright:
-      <a class="text-reset fw-bold" href="https://geocaching.com/">
-        GeoCaching
-      </a>
-    </div>
   </footer>
 );
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,25 +5,17 @@ export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white">
       <div className="container-fluid">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <i className="fas fa-bars"></i>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <Link className="navbar-brand mt-2 mt-lg-0" to="#">
-            <img
-              src="https://www.theredlionpreston.co.uk/wp-content/uploads/Logo_Geocaching_Horiz_Emerald.png"
-              height="15"
-              alt="GeoCaching logo"
-            />
-          </Link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </ button>
+        <Link className="navbar-brand" to="#">
+          <img
+            src="https://www.theredlionpreston.co.uk/wp-content/uploads/Logo_Geocaching_Horiz_Emerald.png"
+            height="15"
+            alt="GeoCaching logo"
+          />
+        </Link>
+        <div className="collapse navbar-collapse" id="navbarCollapse">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <Link className="nav-link" to="#">
@@ -43,8 +35,35 @@ export const Navbar = () => {
           </ul>
         </div>
         <div className="row me-1">
-          <button type="button" className="col btn btn-outline-success">
+          <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" className="col btn btn-outline-success">
             Iniciar sesión
+          </button>
+          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Iniciar sesion</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label">Usuario:</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Add the caption of your post here"></textarea>
+                  </div>
+                  <button class="btn border-secondary" type="button"><i class="fa-solid fa-camera"></i></button>
+                  <button class="btn border-secondary" type="button"><i class="fa-solid fa-location-dot"></i></button>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-success">Publish</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button type="button" className="col btn btn-outline-danger">
+            Registrarse
           </button>
         </div>
       </div>
